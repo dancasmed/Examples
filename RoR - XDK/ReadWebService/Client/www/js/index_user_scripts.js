0241774431a63f57b89c43ceb249369819c351ff
dancasmed <dancasmed@gmail.com>
2015-02-16 22:15:46 -0600

    Devise login example updated
    Just the script was updated

--- RoR - XDK/ReadWebService/Client/www/js/index_user_scripts.js
+++ RoR - XDK/ReadWebService/Client/www/js/index_user_scripts.js
@@ -22,11 +22,12 @@
         lst.innerHTML="";
         var i = 0;
         
-        $.getJSON("http://localhost:3000/list_elements.json", function(data)
+        $.getJSON("http://localhost:3000/elections.json?user_email=usuario1@kratos.org.mx&user_token=ezPFX7zsYYyR4Z-nEWgE", function(data)
         {
             for(i=0; i<data.length; i++)
             {
-                lst.innerHTML+="<a class=\"list-group-item allow-badge widget uib_w_10\" data-uib=\"twitter%20bootstrap/list_item\" data-ver=\"1\"><p class=\"list-group-item-text\">"+data[i]["name"]+"</p><h4 class=\"+list-group-item-heading+\">"+data[i]["description"]+"</h4></a>";
+                console.log(data[i]);
+                lst.innerHTML+="<a class=\"list-group-item allow-badge widget uib_w_10\" data-uib=\"twitter%20bootstrap/list_item\" data-ver=\"1\"><p class=\"list-group-item-text\">"+data[i]["title"]+"</p><h4 class=\"+list-group-item-heading+\">"+data[i]["description"]+"</h4></a>";
             }
         });
         
